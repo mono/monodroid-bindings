830f93d8c5f91848219b1e5e85a6b981ebb1d3de
Jonathan Pryor <jonpryor@vt.edu>
2012-01-26 15:47:58 -0500

    Add binding for Compatibility-v4 library.

---
+++ src/Android.Support.V4.Content/CursorLoader.cs
@@ -0,0 +1,22 @@
+using System;
+using Android.Runtime;
+
+namespace Android.Support.V4.Content {
+
+	public partial class CursorLoader {
+
+		static IntPtr id_loadInBackground;
+		[Register ("loadInBackground", "()Landroid/database/Cursor;", "GetLoadInBackgroundHandler")]
+		public override Java.Lang.Object LoadInBackground ()
+		{
+			if (id_loadInBackground == IntPtr.Zero)
+				id_loadInBackground = JNIEnv.GetMethodID (class_ref, "loadInBackground", "()Landroid/database/Cursor;");
+
+			if (GetType () == ThresholdType)
+				return (Java.Lang.Object) Java.Lang.Object.GetObject<Android.Database.ICursor> (JNIEnv.CallObjectMethod  (Handle, id_loadInBackground), JniHandleOwnership.TransferLocalRef);
+			else
+				return (Java.Lang.Object) Java.Lang.Object.GetObject<Android.Database.ICursor> (JNIEnv.CallNonvirtualObjectMethod  (Handle, ThresholdClass, id_loadInBackground), JniHandleOwnership.TransferLocalRef);
+		}
+	}
+}
+
