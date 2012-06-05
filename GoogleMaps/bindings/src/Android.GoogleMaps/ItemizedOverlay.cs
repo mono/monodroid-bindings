d77ca528426d64e18343ccb4c4492ef4828e2e22
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2012-06-05 12:30:11 +0900

    workaround fix for bogus public setter output and API compatibility issue.

---
+++ GoogleMaps/bindings/src/Android.GoogleMaps/ItemizedOverlay.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Android.GoogleMaps {
+
+	partial class ItemizedOverlay {
+		protected void SetLastFocusedIndex (int value)
+		{
+			LastFocusedIndex = value;
+		}
+	}
+}
+
