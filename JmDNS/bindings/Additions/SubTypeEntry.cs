0bee63ab9033c58e06ce37e729e8c40f49daf63d
Brad Moore <b.moore@4pilabs.com.au>
2014-05-12 12:17:56 +1000

    Few JmDNS changes, still broken.

---
+++ JmDNS/bindings/Additions/SubTypeEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Android.Runtime;
+
+namespace Javax.Jmdns.Impl
+{
+    public partial class SubTypeEntry
+    {
+        /*
+        static IntPtr n_GetKey (IntPtr jnienv, IntPtr native__this)
+        {
+            global::Javax.Jmdns.Impl.JmDNSImpl.SubTypeEntry __this = global::Java.Lang.Object.GetObject<global::Javax.Jmdns.Impl.JmDNSImpl.SubTypeEntry> (jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+            return JNIEnv.NewString(__this.Key.ToString());
+        }
+        */
+    }
+}
+
